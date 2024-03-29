aaa458fa161ab6312f3996e4c355540b9c511d09
JoaoGuilhermeMA <joomoreira36@gmail.com>
2024-03-29 15:37:13 -0300

    conectando ao banco de dados

--- src/main/java/com/ufrn/edu/trabalho/model/Conexao.java
+++ src/main/java/com/ufrn/edu/trabalho/model/Conexao.java
@@ -1,8 +1,17 @@
 package com.ufrn.edu.trabalho.model;
 
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
 
-    public static Conexao getConexao() {
-        String dbUri = System.getenv();
+    public static Connection getConexao() throws SQLException, URISyntaxException {
+        String dbUri = System.getenv("DATABASE_URL");
+        String dbUsername = System.getenv("DATABASE_USERNAME");
+        String dbSenha = System.getenv("DATABASE_SENHA");
+
+        return DriverManager.getConnection(dbUri, dbUsername, dbSenha);
     }
 }
