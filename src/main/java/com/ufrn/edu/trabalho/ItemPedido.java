2617d344a107176a5e098aa5c9c40b4597544264
JoaoGuilhermeMA <joomoreira36@gmail.com>
2024-03-29 16:42:19 -0300

    criando as classes do ItemPedido

---
+++ src/main/java/com/ufrn/edu/trabalho/ItemPedido.java
@@ -0,0 +1,49 @@
+package com.ufrn.edu.trabalho;
+
+public class ItemPedido {
+        private int idItemPedido;
+        private int idPedido;
+        private int idProduto;
+        private int quantidade;
+        private double precoUnitario;
+
+    public int getIdItemPedido() {
+        return idItemPedido;
+    }
+
+    public void setIdItemPedido(int idItemPedido) {
+        this.idItemPedido = idItemPedido;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+}
---
+++ src/main/java/com/ufrn/edu/trabalho/ItemPedidoDAO.java
@@ -0,0 +1,56 @@
+package com.ufrn.edu.trabalho;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemPedidoDAO {
+    private Connection conexao;
+
+    public ItemPedidoDAO(Connection conexao) {
+        this.conexao = conexao;
+    }
+
+    public void inserirItemPedido(ItemPedido itemPedido) throws SQLException {
+        String sql = "INSERT INTO itens_pedido (id_pedido, id_produto, quantidade, preco_unitario) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement pstmt = conexao.prepareStatement(sql)) {
+            pstmt.setInt(1, itemPedido.getIdPedido());
+            pstmt.setInt(2, itemPedido.getIdProduto());
+            pstmt.setInt(3, itemPedido.getQuantidade());
+            pstmt.setDouble(4, itemPedido.getPrecoUnitario());
+            pstmt.executeUpdate();
+        }
+    }
+
+    public List<ItemPedido> buscarItensPorPedido(int idPedido) throws SQLException {
+        List<ItemPedido> itensPedido = new ArrayList<>();
+        String sql = "SELECT * FROM itens_pedido WHERE id_pedido = ?";
+        try (PreparedStatement pstmt = conexao.prepareStatement(sql)) {
+            pstmt.setInt(1, idPedido);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    ItemPedido itemPedido = new ItemPedido();
+                    itemPedido.setIdItemPedido(rs.getInt("id_item_pedido"));
+                    itemPedido.setIdPedido(rs.getInt("id_pedido"));
+                    itemPedido.setIdProduto(rs.getInt("id_produto"));
+                    itemPedido.setQuantidade(rs.getInt("quantidade"));
+                    itemPedido.setPrecoUnitario(rs.getDouble("preco_unitario"));
+                    itensPedido.add(itemPedido);
+                }
+            }
+        }
+        return itensPedido;
+    }
+
+    public void excluirItemPedido(int idItemPedido) throws SQLException {
+        String sql = "DELETE FROM itens_pedido WHERE id_item_pedido = ?";
+        try (PreparedStatement pstmt = conexao.prepareStatement(sql)) {
+            pstmt.setInt(1, idItemPedido);
+            pstmt.executeUpdate();
+        }
+    }
+
+}
