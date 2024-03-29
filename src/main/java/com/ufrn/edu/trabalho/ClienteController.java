e7ad9dbec11fecd30f891d5ca8d7133c220755dc
JoaoGuilhermeMA <joomoreira36@gmail.com>
2024-03-29 16:55:48 -0300

    salvando cliente

---
+++ src/main/java/com/ufrn/edu/trabalho/ClienteController.java
@@ -0,0 +1,17 @@
+package com.ufrn.edu.trabalho;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ClienteController {
+
+    @RequestMapping(value = "/registrar", method = RequestMethod.POST)
+    public void cadastrarCliente(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+}
